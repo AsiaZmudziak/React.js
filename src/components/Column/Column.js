29f61ef289e0802b582d38f6be9094c46b41b879
unknown <asiazmudziak@gmail.com>
2021-01-28 13:24:50 +0100

    add Card component

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,51 @@
 import React from "react";
 import styles from "./Column.scss";
+import Card from "../Card/Card.js";
+import Creator from "../Creator/Creator.js";
 import PropTypes from "prop-types";
+import { settings } from "../../data/dataStore";
 
 class Column extends React.Component {
+    state = {
+        cards: this.props.cards || []
+      };
+
   static propTypes = {
-    title: PropTypes.node.isRequired
+    title: PropTypes.node.isRequired,
+    image: PropTypes.string.isRequired,
+    description: PropTypes.node,
+    columns: PropTypes.array,
+    cards: PropTypes.array,
   };
+
+  addCard(title) {
+    this.setState(state => ({
+      cards: [
+        ...state.cards,
+        {
+          key: state.cards.length ? state.cards[state.cards.length - 1].key + 1 : 0,
+          title,
+          icon: "list-alt"
+          cards: []
+        }
+      ]
+    }));
+  }
 
   render() {
     return (
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.title}></h3>
+         <h3 className={styles.title}>
+          {this.props.title}
+        </h3>
+        <div className={styles.cards}>
+          {this.state.cards.map(({ key, ...cardProps }) => (
+            <Card key={key} {...cardProps} />
+          ))}
+        </div>
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
+        </div>
       </section>
     );
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,7 +5,6 @@ import PropTypes from "prop-types";
 import Column from "../Column/Column.js";
 import Creator from "../Creator/Creator.js";
 import {settings} from '../../data/dataStore';
-import ReactHtmlParser from "react-html-parser";
 
 
 class List extends React.Component {
@@ -45,6 +44,7 @@ class List extends React.Component {
       <section className={styles.component}>
         <Hero titleText={this.props.title} imageImg={this.props.image} />
         <div className={styles.description}>{ReactHtmlParser(this.props.description)}</div>
+        
         <div className={styles.columns}>
         {this.state.columns.map(({ key, ...columnProps }) => (
             <Column key={key} {...columnProps} />
