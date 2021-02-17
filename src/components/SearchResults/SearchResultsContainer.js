eb25c6734b7065083d9616d6d282cbec84c50308
unknown <asiazmudziak@gmail.com>
2021-02-17 19:19:47 +0100

    add search fix cd.

--- src/components/SearchResults/SearchResultsContainer.js
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -3,9 +3,11 @@ import SearchResults from './SearchResults';
 import {createAction_changeSearchString} from '../../redux/searchStringRedux';
 import {getCardsForSearchResults} from '../../redux/cardsRedux';
 
-const mapStateToProps = (state, props) => ({
-  cards: getCardsForSearchResults(state, props.match.params.searchString),
-});
+const mapStateToProps = (state, props) => {
+  console.log(props);
+  const cards = getCardsForSearchResults(state, props.match.params.searchString);
+  return {cards};
+  };
 
 const mapDispatchToProps = (dispatch) => ({
   changeSearchString: newSearchString => dispatch(createAction_changeSearchString(newSearchString)),
